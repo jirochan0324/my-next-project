6d8ad12f94a1329ab930afbcc1413c65bc7a51c8
jiro_chan03 <hayashin0919@gmail.com>
2025-09-08 13:12:24 +0900

    chaenge

--- app/_components/article/index.tsx
+++ app/_components/article/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import type { News } from "@/app/_libs/microcms";
 import Date from "../Date";
@@ -14,8 +15,10 @@ export default function Article({data}:Props){
             <h1 className={styles.title}>{data.title}</h1>
             <p className={styles.description}>{data.description}</p>
             <div className={styles.meta}>
-                <Category category={data.category}></Category>
-                <Date date={data.publishedAt ?? data.createdAt}></Date>
+                <Link href={`/news/category/${data.category.id}`} className={styles.categoryLink}>
+                    <Category category={data.category}/>
+                    <Date date={data.publishedAt ?? data.createdAt}></Date>
+                </Link>
             </div>
             {data.thumbnail&&(
                 <Image
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,18 @@
 import { getNewsList } from "@/app/_libs/microcms";
-import  NewsList from "@/app/_components/NewsList";
+import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
+import { NEWS_LIST_LIMIT } from "@/app/_components/_constants";
+
 
 export default async function Page(){
-  const {contents:news}=await getNewsList();
-  return <NewsList news={news}/>
+  const { contents:news ,totalCount} = await getNewsList({
+    limit:NEWS_LIST_LIMIT,
+  });
+
+  return(
+    <>
+      <NewsList news={news}/>
+      <Pagination totalCount={totalCount}/>
+    </>
+  )
 };
