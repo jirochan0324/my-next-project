4ba2668b972c2e6abdf70f2cf6592b4e538ed936
jiro_chan03 <hayashin0919@gmail.com>
2025-09-09 12:53:50 +0900

    change

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
 
 
-
 export default async function Home() {
 
   const data=await getNewsList({
